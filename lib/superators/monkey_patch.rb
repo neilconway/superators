28e87b535149fc9b48783d41d1fe768425bc6a05
Neil Conway <nrc@cs.berkeley.edu>
2012-03-12 14:45:54 -0700

    More whitespace cleanup.

--- lib/superators/monkey_patch.rb
+++ lib/superators/monkey_patch.rb
@@ -1,26 +1,24 @@
 class Object
-  
   attr_reader :superator_queue
-  
+
   def -@
     extend SuperatorFlag
     @superator_queue ||= []
     @superator_queue.unshift '-'
     self
   end
-  
+
   def +@
     extend SuperatorFlag
     @superator_queue ||= []
     @superator_queue.unshift '+'
     self
   end
-  
+
   def ~@
     extend SuperatorFlag
     @superator_queue ||= []
     @superator_queue.unshift '~'
     self
   end
-  
 end
